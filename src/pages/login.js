1fa742d838f52ea1f084891c029da382439538db
Shazaniyu55 <shazaniyu@gmail.com>
2023-10-31 21:58:03 +0100

    blog

--- src/pages/login.js
+++ src/pages/login.js
@@ -36,7 +36,7 @@ const Login = () => {
   return (
     
       <div className="flex min-h-screen flex-col items-center justify-between p-24 mt-40">
-        <div className="bg-[#212121] p-8 rounded shadow-md w-[500px] h-[500px]">
+        <div className="bg-[#212121] p-8 rounded shadow-md md:w-[500px] h-[500px]">
           <h1 className="text-4xl text-center font-semibold mb-8">Login</h1>
           <form onSubmit={handleSubmit}>
             <input
